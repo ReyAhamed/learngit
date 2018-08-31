63ad8d40d0f520d9dc4fce23159e5427e2588565
Reyaz Ahamed <reyazahamed98@gmail.com>
2018-08-31 22:29:19 +0530

    Added a centralized text

---
+++ quiz/lib/pages/landing_page.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+class LandingPage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context){
+    return new Material(
+      color: Colors.greenAccent,
+      child: new InkWell(
+        child: new Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            new Text("Start the Quizz!!", style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 40.0, fontFamily: 'Caviar')),
+            new Text("So lets start!!", style: new TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 20.0))
+          ],
+        ),
+      ),
+    );
+  }
+}
